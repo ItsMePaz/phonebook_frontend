4a71c829a7e03c99bff5ed9c6a9191899ee463d6
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-07-18 15:19:06 +0800

    Update on form UI

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -7,39 +7,51 @@ function PersonForm({ persons, setPersons }) {
   const [newNumber, setNewNumber] = useState("");
   return (
     <motion.section
-      initial={{ scaleY: "0" }}
-      animate={{ scaleY: "100%" }}
-      transition={{ duration: 0.3 }}
+      initial={{ y: "1000px" }}
+      animate={{ y: "0" }}
+      /*     transition={{ duration: 0.3 }} */
     >
-      <form className="flex flex-col border-solid border-2 px-[5vh] py-[10vh] font-sans">
-        <h1 className="flex justify-center mb-[6vh] mt-[-5vh] font-['Alegreya'] text-3xl">
-          Sign In
+      <form className="flex flex-col border-solid border-2 px-[5vh] rounded-xl border-[#6A8D92] py-[10vh] font-sans bg-[#4B4E6D]">
+        <h1 className="flex justify-center mb-[6vh] mt-[-5vh] font-['Alegreya'] text-3xl bg-[#4B4E6D] text-[#D5E2D0]">
+          Log In
         </h1>
-        <div>
+        <div className="bg-[#4B4E6D]">
           {" "}
           <label className="hidden">Name: </label>
           <input
-            className="border-solid border-2 border-indigo-600 rounded-md p-1"
+            className="border-solid border-2 border-[#6A8D92] rounded-md p-1 bg-[#4B4E6D]"
             type="text"
             value={newPerson}
             placeholder="Username"
             required
           />
         </div>
-        <div>
+        <div className="bg-[#4B4E6D]">
           {" "}
           <label className="hidden">Number:</label>
           <input
-            className="border-solid border-2 border-indigo-600 rounded-md p-1 mt-[3vh]"
+            className="border-solid border-2 border-[#6A8D92] rounded-md p-1 mt-[3vh] bg-[#4B4E6D] "
             type="text"
             value={newNumber}
             placeholder="Password"
             required
           />
         </div>
-        <button className="mt-[3vh] border-solid border-2">Log In</button>
-        <p className="flex justify-center mt-[1vh] text-xs">Or</p>
-        <button className=" border-solid border-2 mt-[1vh]">Sign Up</button>
+        <motion.button
+          whileHover={{ scale: [null, 1.2, 1] }}
+          transition={{ duration: 0.3 }}
+          className="mt-[3vh] border-solid border-2 border-[#6A8D92] bg-[#221B22] rounded-md font-medium text-[#D5E2D0]"
+        >
+          Log In
+        </motion.button>
+        <p className="flex justify-center mt-[1vh] text-xs bg-[#4B4E6D]">Or</p>
+        <motion.div
+          whileHover={{ scale: [null, 1.2, 1] }}
+          transition={{ duration: 0.3 }}
+          className=" border-solid border-2 mt-[1vh] text-center border-[#6A8D92] bg-[#221B22] rounded-md font-medium text-[#D5E2D0]"
+        >
+          Sign Up
+        </motion.div>
       </form>
     </motion.section>
   );
